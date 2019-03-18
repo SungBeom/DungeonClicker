8702a235e0b8fadc9b39880fc3f81d6868c59ecc
deious <gkstjddlekd@naver.com>
2019-03-18 23:47:26 +0900

    Make GameManager
    - Create manager that control the controllers

---
+++ DungeonClicker/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance {
+        get { return instance; }
+    }
+    public Manage manage;
+    
+    void Awake()
+    {
+        if(instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < manage.controller.Length; i++)
+        {
+            manage.controller[i].gameObject.SetActive(true);
+        }
+    }
+
+    [System.Serializable]
+    public class Manage
+    {
+        public GameObject[] controller;
+    }
+}
