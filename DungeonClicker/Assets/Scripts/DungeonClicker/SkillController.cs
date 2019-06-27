20b438966733fd16ca4bd2b58e1df94598344d3d
deious <gkstjddlekd@naver.com>
2019-06-27 23:29:13 +0900

    Fix SkillController
    - Fix SkillController

--- DungeonClicker/Assets/Scripts/DungeonClicker/SkillController.cs
+++ DungeonClicker/Assets/Scripts/DungeonClicker/SkillController.cs
@@ -47,3 +47,80 @@ public class SkillController : MonoBehaviour
         SkillSelected = num;
     }
 }
+
+/// 메인 병합용 스킬 컨트룰러
+/// using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class SkillController : MonoBehaviour
+//{
+//    static int SkillSelected;
+//    public float damage;
+//    int skillCheck;
+//
+//    void Start()
+//    {
+//        damage = DungeonManager.Instance.characterLists[DungeonManager.Instance.cc.selected].SkilDamage[SkillSelected];
+//    }
+//
+//    void OnCollisionEnter2D(Collision2D col)
+//    {
+//        //damage = DungeonManager.Instance.characterLists[DungeonManager.Instance.cc.selected].SkilDamage[SkillSelected]; // 충돌 시 데미지를 들고 오기 때문에 투사체를 던진 후 캐릭터를 변경 시 데미지가 변경될 확률이 높다
+//        skillCheck = DungeonManager.Instance.characterLists[DungeonManager.Instance.cc.selected].SkillCheck[int.Parse(name.Substring(name.Length - 1)) - 1];
+//        Debug.Log(damage);
+//
+//        if (skillCheck == 0)
+//        {
+//            if (col.transform.tag == "Enemy")
+//            {
+//                Debug.Log("단일 기술");
+//                if (col.gameObject.GetComponent<SmallEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<SmallEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<MiddleEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<MiddleEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<LargeEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<LargeEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<BossEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<BossEnemy>().GainDamage(damage);
+//                }
+//                gameObject.SetActive(false);
+//            }
+//        }
+//        else
+//        {
+//            if (col.transform.tag == "Enemy")
+//            {
+//                Debug.Log("범위 기술");
+//                if (col.gameObject.GetComponent<SmallEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<SmallEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<MiddleEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<MiddleEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<LargeEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<LargeEnemy>().GainDamage(damage);
+//                }
+//                else if (col.gameObject.GetComponent<BossEnemy>() != null)
+//                {
+//                    col.gameObject.GetComponent<BossEnemy>().GainDamage(damage);
+//                }
+//            }
+//        }
+//    }
+//
+//    public void Selected(int num)
+//    {
+//        SkillSelected = num;
+//    }
+//}
